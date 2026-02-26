ffb8d68db17a6c684dbd324d172508d1301bfacf
HashimAbbas555 <hashim.abbas@innovative-net.com>
2026-02-26 10:02:07 +0500

    fix: update terminology from 'Voucher' to 'Invoice' in purchase view for consistency

--- resources/views/Purchase/viewPurchaseVoucherListThroughGrn.blade.php
+++ resources/views/Purchase/viewPurchaseVoucherListThroughGrn.blade.php
@@ -36,7 +36,7 @@ $edit = false;
                     <div class="row">
                         <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                             <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12">
-                                <span class="subHeadingLabelClass">View Purchase Voucher List</span>
+                                <span class="subHeadingLabelClass">View Purchase Invoice List</span>
                             </div>
                             <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 text-right">
                                 <?php echo CommonHelper::displayPrintButtonInBlade('PrintEmpExitInterviewList','','1');?>
